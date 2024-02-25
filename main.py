e75f959013aad87d0a8de7289798cddce4381119
d2fLucifer <d2f.working@gmail.com>
2024-02-25 16:20:13 +0700

    Merge changes from optimize_version

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
+
+
 import argparse
-
 from modules.game_state import GameState
-from modules.game_visualization import GameVisualization
 from modules.solver import Solver
-
 
 def load_map(map_path):
     """Load the map from the given path"""
-    f = open(map_path, 'r')
-    map = []
-    for line in f:
-        map.append(list(line.strip()))
-    f.close()
-    return map
-
+    with open(map_path, 'r') as f:
+        game_map = [list(line.strip()) for line in f]
+    return game_map
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--map', help='The map file', default='maps/sokoban1.txt')
-    parser.add_argument(
-        '--strategy', help='The strategy to solve the game', default='greedy')
+    parser.add_argument('--strategy', help='The strategy to solve the game', default='ucs')
     args = parser.parse_args()
 
-    map = load_map(args.map)
+    maps = ["maps/sokoban1.txt", "maps/sokoban2.txt", "maps/sokoban3.txt", "maps/sokoban4.txt"]
+    algorithms = ["bfs", "dfs", "astar", "ucs", "greedy"]
 
-    game_state = GameState(map)
-    strategy = args.strategy
-    solver = Solver(game_state, strategy)
-    solver.solve()
-    solution = solver.get_solution()
-  
-    game_visualization = GameVisualization(game_state, solution)
-    game_visualization.start()
+    for map_path in maps:
+        for algorithm in algorithms:
+            print(f"\nRunning Algorithm: {algorithm} on Map: {map_path}\n")
+            
+            game_map = load_map(map_path)
+            game_state = GameState(game_map)
+            solver = Solver(game_state, algorithm)
+            solver.solve()
+            solution = solver.get_solution()
+            
+            print("\n-----------------------\n")
